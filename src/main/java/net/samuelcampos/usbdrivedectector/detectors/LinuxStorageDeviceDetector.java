5f8bdd7723d86ca55e9dd76de82a8f064bd39c16
samuelcampos <samuelcampos@samuelcampos-VM>
2014-01-10 00:44:29 +0000

    Device detector working on Linux Ubuntu

--- src/main/java/net/samuelcampos/usbdrivedectector/detectors/LinuxStorageDeviceDetector.java
+++ src/main/java/net/samuelcampos/usbdrivedectector/detectors/LinuxStorageDeviceDetector.java
@@ -12,75 +12,120 @@ import net.samuelcampos.usbdrivedectector.USBStorageDevice;
 import org.apache.log4j.Logger;
 
 /**
+ * Tested on Linux Ubuntu 13.10
  *
  * @author samuelcampos
  */
 public class LinuxStorageDeviceDetector extends AbstractStorageDeviceDetector {
-    
-    private static final Logger logger = Logger
-			.getLogger(LinuxStorageDeviceDetector.class);
 
-    private static final String linuxDetectUSBCommand = "system_profiler SPUSBDataType";
-    private static final Pattern linuxPattern = Pattern.compile("^.+Mount Point: (.+)$");
-    
+    private static final Logger logger = Logger
+            .getLogger(LinuxStorageDeviceDetector.class);
+
+    private static final String linuxDetectUSBCommand1 = "df";
+    private static final Pattern command1Pattern = Pattern.compile("^(\\/[^ ]+)[^%]+%[ ]+(.+)$");
+    private static final String linuxDetectUSBCommand2 = "udevadm info -q property -n ";
+    private static final String strDeviceVerifier = "ID_USB_DRIVER=usb-storage";
+
     public LinuxStorageDeviceDetector() {
-        super();   
+        super();
+    }
+
+    private boolean isUSBStorage(String device) {
+        String verifyCommand = linuxDetectUSBCommand2 + device;
+        
+        if (logger.isTraceEnabled()) {
+            logger.trace("Running command: " + verifyCommand);
+        }
+
+        BufferedReader in = null;
+        Process process = null;
+
+        try {
+            process = Runtime.getRuntime().exec(verifyCommand);
+
+            in = new BufferedReader(new InputStreamReader(
+                    process.getInputStream()));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                if(line.equals(strDeviceVerifier))
+                    return true;
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+
+            if (process != null) {
+                process.destroy();
+            }
+        }
+
+        return false;
     }
 
     @Override
     public List<USBStorageDevice> getRemovableDevices() {
         ArrayList<USBStorageDevice> listDevices = new ArrayList<USBStorageDevice>();
 
-		BufferedReader in = null;
-		Process process = null;
+        BufferedReader in = null;
+        Process process = null;
 
-		try {
-			if (logger.isTraceEnabled()) {
-				logger.trace("Running command: " + linuxDetectUSBCommand);
-			}
+        try {
+            if (logger.isTraceEnabled()) {
+                logger.trace("Running command: " + linuxDetectUSBCommand1);
+            }
 
-            /**
-             * system_profiler SPUSBDataType | grep "BSD Name:\|Mount Point:"
-             */
-			process = Runtime.getRuntime().exec(linuxDetectUSBCommand);
+            process = Runtime.getRuntime().exec(linuxDetectUSBCommand1);
 
-			in = new BufferedReader(new InputStreamReader(
-					process.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(
+                    process.getInputStream()));
 
-			String line;
-			while ((line = in.readLine()) != null) {
-                Matcher matcher = linuxPattern.matcher(line);
-				
-                if(matcher.matches()) {
-                    File root = new File(matcher.group(1));
-                    
-                    if (logger.isTraceEnabled()) {
-						logger.trace("Device found: " + root.getPath());
-					}
-                    
-                    
-                    USBStorageDevice device = new USBStorageDevice(root,
-								fsView.getSystemDisplayName(root));
-						listDevices.add(device);
-                        
+            String line;
+            while ((line = in.readLine()) != null) {
+                Matcher matcher = command1Pattern.matcher(line);
+
+                if (matcher.matches()) {
+                    String device = matcher.group(1);
+                    String rootPath = matcher.group(2);
+
+                    if(isUSBStorage(device)) {
+                        File root = new File(rootPath);
+
+                         if (logger.isTraceEnabled()) {
+                         logger.trace("Device found: " + root.getPath());
+                         }
+
+
+                         USBStorageDevice usbDevice = new USBStorageDevice(root,
+                         fsView.getSystemDisplayName(root));
+                         listDevices.add(usbDevice);
+                    }
                 }
-			}
+            }
 
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					logger.error(e.getMessage(), e);
-				}
-			}
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
 
-			if (process != null)
-				process.destroy();
-		}
+            if (process != null) {
+                process.destroy();
+            }
+        }
 
-		return listDevices;
+        return listDevices;
     }
 }
