bb60739e2851ed4f9def74bc048337fc5af12e5e
Samuel Campos <samuelcmp@gmail.com>
2022-03-05 20:29:21 +0000

    Update dependencies + prepare release

---
+++ .github/bashScripts/changelog.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#Simple bash script to generate CHANGELOG.md
+
+git fetch --tags
+LASTTAG=$(git tag -l --sort=v:refname | tail -1)
+COMMITLIST=$(git log --no-merges --format=%s ${LASTTAG}..HEAD)
+PREV_CHANGELOG=$(cat ./CHANGELOG.md)
+RELEASEVERSION=$(mvn -B build-helper:regex-property help:evaluate -Dexpression=newVersion -q -DforceStdout)
+
+echo -e "## Version $RELEASEVERSION\n"  > ./CHANGELOG.md
+IFS=$'\n'; COMMIT_ARRAY=($COMMITLIST); unset IFS;
+for i in "${COMMIT_ARRAY[@]}"
+do
+  echo "- $i"  >> ./CHANGELOG.md
+done
+
+
+echo -e "\n\n" >> ./CHANGELOG.md
+echo "$PREV_CHANGELOG" >> ./CHANGELOG.md
